d47792210e518518529430d535a01b4bcbcf65ba
Kazuma Yoshiyuki <mohamed.ladghem@kinkazma.ga>
2018-05-19 01:59:43 +0200

    Debut Phase 1

--- templates.sh
+++ templates.sh
@@ -19,15 +19,23 @@
 #===============================================================================
 # Jolie mais pas fonctionelle
 # cat /etc/os-release| grep -o -P '(?<=VERSION_ID\=).[0-9]{1,2}(\.[0-9]{1,2})?'
-#COLORS VARS
+
+## COLORS VARS
 RED='\033[0;31m'
 YELLOW='\033[0;33m'
 GREEN='\033[0;32m'
 NC='\033[0m'
-#REGEXP VARS
+
+## REGEXP VARS
 RE_NUMERIC='^[0-9]+$'
-#SHELL RUNTIME
+
+## SHELL RUNTIME
 PS3="Your choice:"
+APP_NAME="APP_SMTP"
+D_TRIGG=false
+## LXC VARS
+#LXC_IP=192.168.113.58/24
+LXC_IP=""
 
 function error(){
 	echo -en "${RED}[ERR]$1${NC}\n"
@@ -38,13 +46,13 @@ function error(){
 }
 
 function info(){
-	echo -en "\033[1m[-]${1}${NC}\n"
+	echo -en "\033[1m[-] ${1}${NC}\n"
 }
 function sucess(){
-	echo -en "${GREEN}[+]$1${NC}\n"
+	echo -en "${GREEN}[+] $1${NC}\n"
 }
 function warn(){
-	echo -en "${YELLOW}[!]$1${NC}\n"
+	echo -en "${YELLOW}[!] $1${NC}\n"
 }
 function debug(){
   if test ${D_TRIGG} = true;then
@@ -64,8 +72,9 @@ function GET_INFO(){
 	OS_VERS=$(cat /etc/os-release| grep -E "^VERSION_ID=" | cut -d "=" -f2)
 	BASH_VERS=$(bash --version | grep -o -E 'version [0-9]{1,2}\.[0-9]{1,2}' | awk '{print $2}')
 	for interface in $(ls /sys/class/net | grep -v lo	);do
-		if test "$(cat /sys/class/net/${interface}/carrier)" = "1";then
+		if test "$(cat /sys/class/net/${interface}/carrier | grep -v -e lo  -e "*br0*" -e "*ovs*" )" = "1";then
 			CONNECTED=true
+			break
 		else
 			CONNECTED=false
 		fi
@@ -75,11 +84,11 @@ function GET_INFO(){
 }
 
 function DEBIAN_PKG_INSTALLED(){
-	dpkg-query -s ${1}  2>/dev/null| grep -q "install ok installed" && return 0 || return 1
+	dpkg-query -s ${1}  2>/dev/null| grep -q "install ok installed" && echo 0 || echo 1
 }
 
 function REDHAT_PKG_INSTALLED(){
-	rpm -q ${1} >/dev/null 2>1 && return 0 || return 1
+	rpm -q ${1} >/dev/null 2>1 && echo  0 || echo 1
 }
 
 function DO_VARS_SETTINGS(){
@@ -95,30 +104,54 @@ function DO_VARS_SETTINGS(){
 		"debian") PKG_INSTALL="apt install -y"; PKG_QUERY="DEBIAN_PKG_INSTALLED"
 			;;
 	esac
-	}
-
-function DEP_CHECK(){
 	declare -a DEPENDENCIES
 	DEPENDENCIES=( lxc )
 	if ! test ${CONNECTED} = true;then
 		error "Not connected to internet"
 	fi
-	for i in ${DESCRIPTION[@]};do 
-		if test $(eval ${PKG_QUERY} ${i}) -ne 0;then
+	for i in ${DEPENDENCIES[@]};do 
+		if test $(eval "${PKG_QUERY} ${i}") -ne 0;then
 			warn "Dependencie ${i} is missing"
-			test ${UID} -eq 0 && info "Installing ${i}" && { eval PKG_INSTALL ${i} | debug; }; || error "Can't install ${i}"
+			test ${UID} -eq 0 && info "Installing ${i}" && { eval "${PKG_INSTALL} ${i}" ; } || error "Can't install ${i}"
 		fi
 	done
+	LXC_VERS=$(lxc-ls --version | cut -c 1)
 	if ! test -r . ;then
 		error "Can't write here"
 	fi
 	if test $(df . --output=pcent |tail -n1|sed 's/%//') -gt 95;then
-		warn "Near to hit the no more space avaible (>97%)"
+		warn "Near to hit the no more space avaible (>95%)"
 	fi
-
-		
-
-
-
 			
 }
+
+function PHASE1(){
+	info "PHASE1 Starting"
+	info "Creation du container ${APP_NAME}"
+	lxc-create -n ${APP_NAME} -t download -- -d ubuntu -r bionic -a amd64 >/dev/null 2>&1
+	if test ${UID} -eq 0 ;then 
+		LXC_PATH=$(grep -oP '(?<=lxc.lxcpath=).*' /etc/lxc/lxc.conf)
+	else
+		LXC_PATH=$(grep -oP '(?<=lxc.lxcpath=).*' ${HOME}/.config/lxc/lxc.conf)
+	fi
+	APP_PATH=${LXC_PATH}/${APP_NAME}
+	APP_CONFIG=${APP_PATH}/config
+	APP_ROOTFS=${APP_PATH}/rootfs
+	if ! test -z ${LXC_IP};then	
+		case "${LXC_VERS}" in
+			2) LXC_NET="lxc.network.ipv4.address = ${LXC_IP}" ;;
+			3) LXC_NET="lxc.net.0.ipv4.address = ${LXC_IP}" ;;
+		esac
+		echo "${LXC_NET}" >> ${APP_CONFIG}
+	fi
+
+}
+
+function MAIN(){
+GET_INFO
+DO_VARS_SETTINGS
+PHASE1
+}
+
+MAIN
+exit 0
