aa27d554ee6297e8f1e77b9765862c003997ad34
Kazuma Yoshiyuki <mohamed.ladghem@kinkazma.ga>
2018-06-14 02:37:11 +0200

    update mg

--- MailGarbage/form.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField,SubmitField
-from wtforms.validators import DataRequired,Length,Email
-
-class OpenMailBox(FlaskForm):
-    mail_address = StringField('Email',validators=[ DataRequired("Ce champ est requis !"),Length(min=5,max=20, message="La taille de l'addresse doit etre entre 5 et 20 char"),Email("Ce n'est pas un mail !") ])
-    submit = SubmitField('Validation')
-
